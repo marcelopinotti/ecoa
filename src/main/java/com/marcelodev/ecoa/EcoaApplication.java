c4e03372ddc91e6f97be6097e8124d19971a7a7c
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-09-11 17:04:01 -0300

    build: configure local app

--- src/main/java/com/marcelodev/ecoa/EcoaApplication.java
+++ src/main/java/com/marcelodev/ecoa/EcoaApplication.java
@@ -2,8 +2,10 @@ package com.marcelodev.ecoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EcoaApplication {
 
     public static void main(String[] args) {
